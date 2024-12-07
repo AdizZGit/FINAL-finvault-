e3d533ef1b2ba806a7dbc8d2d31863ee49be0fd7
forUdayraj <for.udayraj@gmail.com>
2024-12-07 13:24:02 +0530

    user side credit card

--- src/pages/UserCredit.jsx
+++ src/pages/UserCredit.jsx
@@ -3,22 +3,24 @@ import './UserCredit.css';
 
 export default function UserCredit() {
   return (
-    <div className="add-card-settings">
-      <div className="add-card">
-        <h3>Apply for credit card</h3>
-        <form>
-          <label>Card Type</label>
-          <input type="text" placeholder="Classic" />
-          <label>Name On Card</label>
-          <input type="text" placeholder="Enter your Name" />
-          <input type="date" />
-          <button type="submit">Add Card</button>
-        </form>
+    <div>
+      <div className="add-card-settings">
+        <div className="add-card">
+          <h3>Apply for credit card</h3>
+          <form>
+            <label>Card Type</label>
+            <input type="text" placeholder="Classic" />
+            <label>Name On Card</label>
+            <input type="text" placeholder="Enter your Name" />
+            <input type="date" />
+            <button type="submit">Add Card</button>
+          </form>
+        </div>
+        <div className='card-settings'>
+          <h3>Your cards</h3>
+        </div>
       </div>
-      <div className='card-settings'>
-       <h3>Your cards</h3>
-      </div>
-      <div className="card-settings">
+      <div className="card-settings-1">
         <h3>Card Settings</h3>
         <ul>
           <li>Block Card</li>
